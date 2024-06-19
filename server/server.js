49a6432165839a474c8421283512bce82eb9ded9
Philip Henning <philiphenning@outlook.com>
2024-06-19 14:43:02 +0100

    added query to get all books

--- server/server.js
+++ server/server.js
@@ -19,6 +19,16 @@ app.get('/', (request, response) => {
     response.json({message: `Root for Wednesday`})
 })
 
+app. get('/books', async (request, response) => {
+    try{
+        const data = await db.query("SELECT * FROM books")
+        response.json(data)    
+    }
+     catch (error){
+        response.json(error)
+     }
+})
+
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`)
 })
